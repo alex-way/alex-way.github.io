ab22c9e641dc1d595aa7f84b7f4576707514c6e8
Alex Way <alexwa@softcat.com>
2022-05-23 19:33:35 +0100

    Add missing configuration to use gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -14,6 +16,9 @@ const config = {
   ],
   kit: {
     adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/alex-way.github.io'
+    },
     prerender: { default: true }
   }
 };
