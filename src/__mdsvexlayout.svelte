c74895061c6959af508133668d838b220bb8ebe6
Alex Way <alexwa@softcat.com>
2022-05-23 19:13:03 +0100

    Remove unrequired packages Update readme

--- src/__mdsvexlayout.svelte
+++ src/__mdsvexlayout.svelte
@@ -1,20 +1,20 @@
 <script context="module">
-  import h1 from '$lib/components/cv/components/base/h1.svelte';
-  import h2 from '$lib/components/cv/components/base/h2.svelte';
-  import h3 from '$lib/components/cv/components/base/h3.svelte';
-  import h4 from '$lib/components/cv/components/base/h4.svelte';
-  import h5 from '$lib/components/cv/components/base/h5.svelte';
-  import h6 from '$lib/components/cv/components/base/h6.svelte';
-  import p from '$lib/components/cv/components/base/p.svelte';
-  import li from '$lib/components/cv/components/base/li.svelte';
-  import ul from '$lib/components/cv/components/base/ul.svelte';
-  import a from '$lib/components/cv/components/base/a.svelte';
-  import code from '$lib/components/cv/components/base/code.svelte';
-  import em from '$lib/components/cv/components/base/em.svelte';
-  import strong from '$lib/components/cv/components/base/strong.svelte';
-  import hr from '$lib/components/cv/components/base/hr.svelte';
-  import td from '$lib/components/cv/components/base/td.svelte';
-  import table from '$lib/components/cv/components/base/table.svelte';
+  import h1 from '$lib/components/cv/h1.svelte';
+  import h2 from '$lib/components/cv/h2.svelte';
+  import h3 from '$lib/components/cv/h3.svelte';
+  import h4 from '$lib/components/cv/h4.svelte';
+  import h5 from '$lib/components/cv/h5.svelte';
+  import h6 from '$lib/components/cv/h6.svelte';
+  import p from '$lib/components/cv/p.svelte';
+  import li from '$lib/components/cv/li.svelte';
+  import ul from '$lib/components/cv/ul.svelte';
+  import a from '$lib/components/cv/a.svelte';
+  import code from '$lib/components/cv/code.svelte';
+  import em from '$lib/components/cv/em.svelte';
+  import strong from '$lib/components/cv/strong.svelte';
+  import hr from '$lib/components/cv/hr.svelte';
+  import td from '$lib/components/cv/td.svelte';
+  import table from '$lib/components/cv/table.svelte';
 
   export { h1, h2, h3, h4, h5, h6, p, li, ul, a, code, em, strong, hr, td, table };
 </script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
-
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true
@@ -16,12 +13,7 @@ const config = {
     mdsvex(mdsvexConfig)
   ],
   kit: {
-    adapter: adapter(),
-
-    // Override http methods in the Todo forms
-    methodOverride: {
-      allowed: ['PATCH', 'DELETE']
-    }
+    adapter: adapter()
   }
 };
 
